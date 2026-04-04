b8f9c1bdb94e07844578e4d8b060b2291720769f
Vinicius <165406865+CallMeVinBR@users.noreply.github.com>
2026-04-03 23:36:08 -0300

    Add files via upload

---
+++ run.py
@@ -0,0 +1,37 @@
+import os
+
+def calculateSeconds(hours=0, minutes=0, seconds=0) -> int:
+   totalSeconds = (hours * (60 ** 2)) + (minutes * 60) + seconds
+   
+   return totalSeconds
+
+print("TIMED SHUTDOWN HELPER".center(50, "="))
+print(">> Press CTRL + C to close and cancel the program <<\n\n")
+
+print("Countdown".center(25, "-"))
+
+try:
+   hours = int(input("Hours: "))
+   minutes = int(input("Minutes: "))
+   seconds = int(input("Seconds: "))
+      
+   if hours < 0: hours = 0
+   if minutes < 0: minutes = 0
+   if seconds < 0: seconds = 0
+      
+   print(f"Informed time: {hours}h {minutes}m {seconds}s")
+   confirmed = str(input("\nProceed to creating the shutdown? (y/n)\n> ")).lower().strip()
+      
+   if confirmed == "y":
+      if hours == 0 and minutes == 0 and seconds == 0:
+         confirmed = str(input("\nWARNING: Creating a shutdown with no countdown results in an instant shutdown. Do you REALLY want do proceed? (y/n)\n> ")).lower().strip()
+            
+         if confirmed == "y":
+            os.system('shutdown /s /f /t 0')
+         
+      countdown = calculateSeconds(hours, minutes, seconds)
+      os.system(f'shutdown /s /t {countdown}')
+      
+except:
+   print("\nPlease inform the time in Integer numbers, not Decimals.")
+   input("Press ENTER to end the execution.")
